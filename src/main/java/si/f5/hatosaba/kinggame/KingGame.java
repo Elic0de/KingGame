4e004e1fd4d3bfbc29400f24f1047dfd8dc37149
oieii <lpondboy@gmail.com>
2021-11-26 14:31:15 +0900

    first commit

---
+++ src/main/java/si/f5/hatosaba/kinggame/KingGame.java
@@ -0,0 +1,19 @@
+package si.f5.hatosaba.kinggame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KingGame extends JavaPlugin {
+
+    private static KingGame instance;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
